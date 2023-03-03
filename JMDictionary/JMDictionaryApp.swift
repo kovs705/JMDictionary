86af05109feaa31eb35c45946a4c6dcb7d62bba6
kovs705 <smartkovsik@gmail.com>
2023-03-03 16:36:04 +0300

    Initial Commit

---
+++ JMDictionary/JMDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JMDictionaryApp.swift
+//  JMDictionary
+//
+//  Created by Kovs on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct JMDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
