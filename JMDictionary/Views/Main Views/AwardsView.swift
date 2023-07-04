332694e7d5413ed4a514d0a8289575cadd91ad7c
kovs705 <smartkovsik@gmail.com>
2023-07-04 20:44:52 +0300

    added awards json for russian and english languages, view and model in addition

--- JMDictionary/Views/Main Views/AwardsView.swift
+++ JMDictionary/Views/Main Views/AwardsView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct AwardsView: View {
+    static let tag: String? = "Awards"
+    @EnvironmentObject var dataController: DataController
+
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(Award.allAwards) { award in
+                        Button {
+                            selectedAward = award
+                            showingAwardDetails = true
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(dataController.hasEarned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Awards")
+            .alert(isPresented: $showingAwardDetails) {
+                if dataController.hasEarned(award: selectedAward) {
+                    return Alert(
+                        title: Text("Unlocked: \(selectedAward.name)"),
+                        message: Text(selectedAward.description),
+                        dismissButton: .default(Text("OK")))
+                } else {
+                    return Alert(title: Text("Locked"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+                }
+            }
+        }
     }
 }
 
@@ -18,3 +57,4 @@ struct AwardsView_Previews: PreviewProvider {
         AwardsView()
     }
 }
+
